262318c2f4ee07864b3b7f57cf0783b16961af44
Matt McCarthy <matt_22_mccarthy@live.com>
2021-07-17 17:06:52 -0400

    finished i/o handling question

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,56 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP1)
+
+    // n = 0
+    @n
+    M = 0
+
+(LOOP2)
+
+    // if (SCREEN + n > 24575) jump to LOOP1
+    @SCREEN
+    D=A
+    @n
+    D=D+M
+    @24575
+    D=D-A
+    @LOOP1
+    D;JGT
+
+    // get the current row
+    @SCREEN
+    D=A
+    @n
+    D=D+M
+    @curr
+    M=D
+
+    // if (*KBD == 0), set current row to white
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ
+
+    // otherwise, set the current row to black
+    @curr
+    A=M
+    M=-1
+
+(INC)
+
+    // increment n and jump to LOOP2
+    @n
+    M=M+1
+    @LOOP2
+    0;JMP
+
+(WHITE)
+
+    // set current row to white and jump to INC
+    @curr
+    A=M
+    M=0
+    @INC
+    0;JMP
