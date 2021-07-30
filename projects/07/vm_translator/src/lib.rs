f7249d8e8287dda876ab830cff5a19aafdb9f629
Matt McCarthy <matt_22_mccarthy@live.com>
2021-07-30 15:05:29 -0400

    worked on vm translator

--- projects/07/vm_translator/src/lib.rs
+++ projects/07/vm_translator/src/lib.rs
@@ -3,384 +3,6 @@ use std::error::Error;
 use std::path::Path;
 use std::ffi::OsStr;
 
-pub struct Config {
-  file_stem: String,
-  input_filename: String,
-  output_filename: String,
-}
-
-impl Config {
-  pub fn new (args: &[String]) -> Result<Config, &str> {
-      if args.len() < 2 {
-          return Err("not enough arguments");
-      }
-
-      let input_filename = args[1].clone();
-
-      let extension = Path::new(&input_filename)
-        .extension()
-        .and_then(OsStr::to_str);
-
-      if let Some(e) = extension {
-        if e != "vm" {
-          return Err("file must have .vm extension");
-        }
-      } else {
-        return Err("no file extension");
-      }
-
-      let file_stem = String::from(
-        Path::new(&input_filename)
-        .file_stem()
-        .and_then(OsStr::to_str)
-        .unwrap());
-
-      let output_filename = format!("{}.asm", file_stem);
-
-      Ok(Config {
-        file_stem,
-        input_filename,
-        output_filename,
-      })
-  }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  let input = fs::read_to_string(config.input_filename)?;
-
-  let instructions = get_instructions(input);
-
-  let output = generate_output(&instructions, config.file_stem);
-
-  Ok(())
-}
-
-fn push() -> &'static str {
-"\
-@SP
-A=M
-M=D
-@SP
-M=M+1
-"
-}
-
-fn pop() -> &'static str {
-"\
-@SP
-AM=M-1
-D=M
-"
-}
-
-fn load_constant(val: &str) -> String {
-  format!(
-"\
-@{}
-D=A
-", val)
-}
-
-fn load_static(val: &str, file_stem: &str) -> String {
-  format!(
-"\
-@{}.{}
-D=M
-", file_stem, val)
-}
-
-fn load_local(val: &str) -> String {
-  format!(
-"\
-@{}
-D=A
-@LCL
-A=M
-A=A+D
-D=M
-", val)
-}
-
-fn pop_local(val: &str) -> String {
-  format!(
-"\
-@{}
-D=A
-@LCL
-A=M
-D=D+A
-@R0
-M=D
-@SP
-AM=M-1
-D=M
-@R0
-A=M
-M=D
-", val)
-}
-
-fn pop_argument(val: &str) -> String {
-  format!(
-"\
-@{}
-D=A
-@ARG
-A=M
-D=D+A
-@R0
-M=D
-@SP
-AM=M-1
-D=M
-@R0
-A=M
-M=D
-", val)
-}
-
-fn pop_that(val: &str) -> String {
-  format!(
-"\
-@{}
-D=A
-@THAT
-A=M
-D=D+A
-@R0
-M=D
-@SP
-AM=M-1
-D=M
-@R0
-A=M
-M=D
-", val)
-}
-
-fn pop_this(val: &str) -> String {
-  format!(
-"\
-@{}
-D=A
-@THIS
-A=M
-D=D+A
-@R0
-M=D
-@SP
-AM=M-1
-D=M
-@R0
-A=M
-M=D
-", val)
-}
-
-fn load_argument(val: &str) -> String {
-  format!(
-"\
-@{}
-D=A
-@ARG
-A=M
-A=A+D
-D=M
-", val)
-}
-
-fn load_that(val: &str) -> String {
-  format!(
-"\
-@{}
-D=A
-@THAT
-A=M
-A=A+D
-D=M
-", val)
-}
-
-
-fn load_this(val: &str) -> String {
-  format!(
-"\
-@{}
-D=A
-@THIS
-A=M
-A=A+D
-D=M
-", val)
-}
-
-fn load_pointer(val: &str) -> &str {
-  if val == "0" {
-"\
-@THIS
-D=M
-"
-  } else {
-"\
-@THAT
-D=M
-"
-  }
-}
-
-fn load_temp(val: &str) -> String {
-  format!(
-"\
-@5
-D=A
-@{}
-A=A+D
-D=M
-", val)
-}
-
-fn save_static(val: &str, file_stem: &str) -> String {
-  format!(
-"\
-@{}.{}
-M=D
-", file_stem, val)
-}
-
-fn generate_output(instructions: &Vec<Instruction>, file_stem: String) -> String {
-  let mut output = String::new();
-
-  for instruction in instructions {
-    match instruction {
-      Instruction::Movement(movement) => {
-        match movement {
-          Movement::Push(segment, val) => {
-            match segment {
-              Segment::Argument => {
-                output.push_str(&format!("{}{}", load_argument(val), push()));
-              },
-              Segment::Local    => {
-                output.push_str(&format!("{}{}", load_local(val), push()));
-              },
-              Segment::Static   => {
-                output.push_str(&format!("{}{}", load_static(val, &file_stem), push()));
-              },
-              Segment::Constant => {
-                output.push_str(&format!("{}{}", load_constant(val), push()));
-              },
-              Segment::This     => {
-                output.push_str(&format!("{}{}", load_this(val), push()));
-              },
-              Segment::That     => {
-                output.push_str(&format!("{}{}", load_that(val), push()));
-              },
-              Segment::Pointer  => {
-                output.push_str(&format!("{}{}", load_pointer(val), push()));
-              },
-              Segment::Temp     => {
-                output.push_str(&format!("{}{}", load_temp(val), push()));
-              },
-            }
-          },
-          Movement::Pop(segment, val) => {
-            match segment {
-              Segment::Argument => {
-                output.push_str(&pop_argument(val));
-              },
-              Segment::Local    => {
-                output.push_str(&pop_local(val));
-              },
-              Segment::Static   => {
-                output.push_str(&format!("{}{}", pop(), save_static(val, &file_stem)));
-              },
-              Segment::This     => {
-                output.push_str(&pop_this(val));
-              },
-              Segment::That     => {
-                output.push_str(&pop_that(val));
-              },
-              Segment::Pointer  => {
-
-              },
-              Segment::Temp     => {
-
-              },
-              _ => continue,
-            }
-          }
-        }
-      },
-      Instruction::Calculation(calculation) => {
-        match calculation {
-          Calculation::Arithmetic(arithmetic) => {
-            match arithmetic {
-              Arithmetic::Add => {
-
-              },
-              Arithmetic::Sub => {
-
-              },
-              Arithmetic::Neg => {
-
-              },
-            }
-          },
-          Calculation::Comparison(comparison) => {
-            match comparison {
-              Comparison::Eq => {
-
-              },
-              Comparison::Gt => {
-  
-              },
-              Comparison::Lt => {
-  
-              },
-            }
-          },
-          Calculation::Logical(logical) => {
-            match logical {
-              Logical::And => {
-
-              },
-              Logical::Or  => {
-
-              },
-              Logical::Not => {
-                
-              }
-            }
-          },
-        }
-      },
-    }
-  }
-
-  output
-}
-
-// Get the instructions out of a file
-fn get_instructions(input: String) -> Vec<Instruction> {
-  let mut result = Vec::new();
-
-  for line in input.split('\n') {
-    let instruction = strip_line(line);
-    if !instruction.is_empty() {
-      result.push(Instruction::get(instruction));
-  }
-    }
-    
-  result
-}
-
-// Strips comments and whitespace from a line.
-fn strip_line(line: &str) -> &str {
-  match line.find("//") {
-    Some(index) => line.get(..index).unwrap().trim(),
-    None        => line.trim()
-  }
-}
-
 enum Segment {
   Argument,
   Local,
@@ -504,4 +126,299 @@ impl Instruction {
       None => Instruction::Calculation(Calculation::get(s))
     }
   }
+}
+
+// Get the instructions out of a file
+fn get_instructions(input: String) -> Vec<Instruction> {
+  let mut result = Vec::new();
+
+  for line in input.split('\n') {
+    let instruction = strip_line(line);
+    if !instruction.is_empty() {
+      result.push(Instruction::get(instruction));
+  }
+    }
+    
+  result
+}
+
+// Strips comments and whitespace from a line.
+fn strip_line(line: &str) -> &str {
+  match line.find("//") {
+    Some(index) => line.get(..index).unwrap().trim(),
+    None        => line.trim()
+  }
+}
+
+pub struct Config {
+  file_stem: String,
+  input_filename: String,
+  output_filename: String,
+}
+
+impl Config {
+  pub fn new (args: &[String]) -> Result<Config, &str> {
+      if args.len() < 2 {
+          return Err("not enough arguments");
+      }
+
+      let input_filename = args[1].clone();
+
+      let extension = Path::new(&input_filename)
+        .extension()
+        .and_then(OsStr::to_str);
+
+      if let Some(e) = extension {
+        if e != "vm" {
+          return Err("file must have .vm extension");
+        }
+      } else {
+        return Err("no file extension");
+      }
+
+      let file_stem = String::from(
+        Path::new(&input_filename)
+        .file_stem()
+        .and_then(OsStr::to_str)
+        .unwrap());
+
+      let output_filename = format!("{}.asm", file_stem);
+
+      Ok(Config {
+        file_stem,
+        input_filename,
+        output_filename,
+      })
+  }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  let input = fs::read_to_string(config.input_filename)?;
+
+  let instructions = get_instructions(input);
+
+  let output = generate_output(&instructions, config.file_stem);
+
+  Ok(())
+}
+
+fn generate_output(instructions: &Vec<Instruction>, file_stem: String) -> String {
+  let mut output = String::new();
+
+  for instruction in instructions {
+    match instruction {
+      Instruction::Movement(movement) => {
+        match movement {
+          Movement::Push(segment, val) => {
+            match segment {
+              Segment::Argument => output.push_str(push_argument(val)),
+              Segment::Local    => output.push_str(push_local(val)),
+              Segment::Static   => output.push_str(push_static(val, file_stem)),
+              Segment::Constant => output.push_str(push_constant(val)),
+              Segment::This     => output.push_str(push_this(val)),
+              Segment::That     => output.push_str(push_that(val)),
+              Segment::Pointer  => output.push_str(push_pointer(val)),
+              Segment::Temp     => output.push_str(push_temp(val)),
+            }
+          },
+          Movement::Pop(segment, val) => {
+            match segment {
+              Segment::Argument => output.push_str(pop_argument(val)),
+              Segment::Local    => output.push_str(pop_local(val)),
+              Segment::Static   => output.push_str(pop_static(val, file_stem)),
+              Segment::This     => output.push_str(pop_this(val)),
+              Segment::That     => output.push_str(pop_that(val)),
+              Segment::Pointer  => output.push_str(pop_pointer(val)),
+              Segment::Temp     => output.push_str(pop_temp(val)),
+              _ => continue,
+            }
+          }
+        }
+      },
+      Instruction::Calculation(calculation) => {
+        match calculation {
+          Calculation::Arithmetic(arithmetic) => {
+            match arithmetic {
+              Arithmetic::Add => output.push_str(add()),
+              Arithmetic::Sub => output.push_str(sub()),
+              Arithmetic::Neg => output.push_str(neg()),
+            }
+          },
+          Calculation::Comparison(comparison) => {
+            match comparison {
+              Comparison::Eq => output.push_str(eq()),
+              Comparison::Gt => output.push_str(gt()),
+              Comparison::Lt => output.push_str(lt()),
+            }
+          },
+          Calculation::Logical(logical) => {
+            match logical {
+              Logical::And => output.push_str(and()),
+              Logical::Or  => output.push_str(or()),
+              Logical::Not => output.push_str(not()),
+            }
+          },
+        }
+      },
+    }
+  }
+  output
+}
+
+fn push_argument(offset: &str) -> &str {
+  let result = String::new();
+  result.push_str(format!("@{}\n", offset));
+  result.push_str(
+"\
+D=A
+@ARG
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+"
+  );
+
+  &result
+}
+
+fn push_local(offset: &str) -> &str {
+  let result = String::new();
+  result.push_str(format!("@{}\n", offset));
+  result.push_str(
+"\
+D=A
+@LCL
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+"
+  );
+    
+  &result
+}
+
+fn push_this(offset: &str) -> &str {
+  let result = String::new();
+  result.push_str(format!("@{}\n", offset));
+  result.push_str(
+"\
+D=A
+@THIS
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+"
+  );
+    
+  &result
+}
+
+fn push_that(offset: &str) -> &str {
+  let result = String::new();
+  result.push_str(format!("@{}\n", offset));
+  result.push_str(
+"\
+D=A
+@THAT
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+"
+  );
+    
+  &result
+}
+
+fn push_this_pointer() -> &'static str {
+"\
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+"
+}
+
+fn push_that_pointer() -> &'static str {
+"\
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+"
+}
+
+fn push_temp(offset: &str) -> &str {
+  let result = String::new();
+  result.push_str(format!("@{}\n", offset));
+  result.push_str(
+"\
+D=A
+@5
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+"
+  );
+    
+  &result
+}
+
+fn push_constant(val: &str) -> &str {
+  let result = String::new();
+  result.push_str(format!("@{}\n", val));
+  result.push_str(
+"\
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+"
+  );
+
+  &result
+}
+
+fn push_static(val: &str, file_stem: &str) -> &str {
+  let result = String::new();
+  result.push_str(format!("@{}.{}\n", val, file_stem));
+  result.push_str(
+"\
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+"
+  );
+
+  &result
 }
